06999b5c4b5bc1b96062ee7145e665e3005bfdfd
yaseen s <ycs613678@gmail.com>
2025-05-01 17:58:46 +0530

    methods added,packages added,inner class added

--- GamePanel.java
+++ GamePanel.java
@@ -1,13 +1,42 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+	public GamePanel() {
+		// TODO Auto-generated constructor stub
+	}
+	public void startGame() {
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkcollision() {
+		
+	}
+	public void gameover(Graphics g) {
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+	public class MykeyAdaptor extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 		
 	}
 
