1d84b1f16ed27667d58bc7a7adea68da6c854338
yaseen s <ycs613678@gmail.com>
2025-05-01 23:31:59 +0530

    variables declared

--- GamePanel.java
+++ GamePanel.java
@@ -5,6 +5,21 @@ import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+	static final int SCREEN_WIDTH=600;
+	static final int SCREEN_HEIGHT=600;
+	static final int UNIT_SIZE=25;
+	static final int GAME_UNITS=SCREEN_HEIGHT*SCREEN_WIDTH/UNIT_SIZE;
+	static final int DELAY=75;
+	final int x[]=new int[GAME_UNITS];
+	final int y[]=new int[GAME_UNITS];
+	int bodyparts=6;
+	int appleseaten;
+	int appleX;
+	int appleY;
+	char direction='R';
+	boolean running=false;
+	Timer timer;
+	Random random;
 	public GamePanel() {
 		// TODO Auto-generated constructor stub
 	}
