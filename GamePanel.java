0fd83b7b008b82285c4c6d76882f0c3acf2a9572
yaseen s <ycs613678@gmail.com>
2025-05-01 17:03:25 +0530

    frame added

--- GamePanel.java
+++ GamePanel.java
@@ -1,4 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GamePanel {
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
