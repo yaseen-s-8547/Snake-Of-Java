75ce96f8058f5c3556ce8c2cdf57c3562e66c7d6
yaseen s <ycs613678@gmail.com>
2025-05-05 12:48:05 +0530

    constructor defined,startgame method defined,gridlines added

--- GamePanel.java
+++ GamePanel.java
@@ -21,15 +21,37 @@ public class GamePanel extends JPanel implements ActionListener {
 	Timer timer;
 	Random random;
 	public GamePanel() {
-		// TODO Auto-generated constructor stub
+		random=new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MykeyAdaptor());
+		startGame();
+		
+		
 	}
 	public void startGame() {
+		newApple();
+		running=true;
+		timer=new Timer(DELAY,this);
+		timer.start();
+		
 		
 	}
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
 		
 	}
 	public void draw(Graphics g) {
+		for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+			
+		}
+		
+	}
+	public void newApple() {
 		
 	}
 	public void move() {
