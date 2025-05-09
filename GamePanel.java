b5715bca7edfa7516b2f3091f5f94f8ad4920318
yaseen s <ycs613678@gmail.com>
2025-05-10 00:58:03 +0530

    added apple

--- GamePanel.java
+++ GamePanel.java
@@ -49,9 +49,13 @@ public class GamePanel extends JPanel implements ActionListener {
 			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
 			
 		}
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 		
 	}
 	public void newApple() {
+		appleX=random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleY=random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 		
 	}
 	public void move() {
